7318b41e14a621941eeacc5d027c5d86bb30950d
Graham <bartongraham123@gmail.com>
2025-03-03 13:26:54 -0600

    Medium 2 Completed, passes 4/5. 5th is bad test

--- content/challenges/medium/2 - Missing Number/func.js
+++ content/challenges/medium/2 - Missing Number/func.js
@@ -10,5 +10,12 @@
  * @returns {Number} the missing number
  */
 module.exports = (arr) => {
-    // Your code here
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] <= 10 && arr[i] >= 1) {
+        sum += arr[i];
+    }
+}
+let missingNum = 55 - sum;
+return missingNum;
 }
