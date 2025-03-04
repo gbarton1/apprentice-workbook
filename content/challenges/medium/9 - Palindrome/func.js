6ebb61b6d521ee0ed97206ca8c56d4fba110039a
Graham <bartongraham123@gmail.com>
2025-03-04 10:24:18 -0600

    medium 9 completed

--- content/challenges/medium/9 - Palindrome/func.js
+++ content/challenges/medium/9 - Palindrome/func.js
@@ -9,5 +9,16 @@
  * @returns {boolean}
  */
 module.exports = (str) => {
-    // Your code here
+    if (str.length == 0) {
+        return true;
+    };
+    for (let i = 0; i < str.length; i++) {
+        for (let k = str.length - 1; k >= 0; k--) {
+            if (str[i] === str[k]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
